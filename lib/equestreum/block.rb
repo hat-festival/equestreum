f08d84b4e08c3ff55d54aa4e1181dc6117743b4f
pikesley <sam.pikesley@gmail.com>
2018-05-27 21:49:25 +0100

    Mine a block

--- lib/equestreum/block.rb
+++ lib/equestreum/block.rb
@@ -8,5 +8,18 @@ module Equestreum
 
       yield self if block_given?
     end
+
+    def mine
+      @time = Time.now.to_i
+      @nonce = 0
+      loop do
+        @hash = Equestreum.hash @nonce, @difficulty, @prev, @data
+        if Equestreum.difficulty_attained hash, @difficulty
+          break
+        else
+          @nonce += 1
+        end
+      end
+    end
   end
 end
--- spec/block_spec.rb
+++ spec/block_spec.rb
@@ -19,6 +19,23 @@ module Equestreum
 
         expect(block.difficulty).to eq '0000'
       end
+
+      it 'mines itself' do
+        block = Block.new do |b|
+          b.data = 'equestreum'
+          b.prev = '00005a04bcaba76ec015e6626b417b61874562c7b35dc4e982f413a0b8c47336'
+          b.difficulty = '0'
+        end
+
+        Timecop.freeze '1974-06-15' do
+          block.mine
+        end
+
+        expect(block.hash).to eq '03b3e7d06e697ce00a58664e5f56f527a89bbd3f877e1ebd90deb278cb7cc9d2'
+        expect(block.nonce).to eq 9
+        expect(block.data).to eq 'equestreum'
+        expect(block.time).to eq 140482800
+      end
     end
   end
 end
