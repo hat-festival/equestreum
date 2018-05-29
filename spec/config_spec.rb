61547975bb82122774ae7b02aafb1151bd29b3b0
pikesley <sam.pikesley@gmail.com>
2018-05-29 19:09:46 +0100

    Fix up config

--- spec/config_spec.rb
+++ spec/config_spec.rb
@@ -1,11 +1,12 @@
 module Equestreum
   describe Config do
-    before :each do
-      @conf = Equestreum::Config.instance.config
-    end
-
     it 'has a path' do
-      expect(@conf['chain_path']).to eq 'tmp/equestreum.chain'
+      fixture_yaml = YAML.load_file 'spec/fixtures/config.yaml'
+      allow(YAML).to receive(:load_file).and_call_original
+      allow(YAML).to receive(:load_file).
+        with("#{ENV['HOME']}/.equestreum/config.yaml").and_return(fixture_yaml)
+      @conf = Equestreum::Config.instance.config
+      expect(@conf['chain_path']).to eq '/home/horse/.equestreum/equestreum.chain'
     end
   end
 end
